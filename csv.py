c458128b2cfdc076ed07334c6eab03d1bf3e6438
Bettalion <Bettalion04@hotmail.com>
2021-12-20 13:56:42 +0000

    added storing and reading capability

---
+++ csv.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+# a = numpy.asarray(list(range(10)))
+# numpy.savetxt("x.csv", a, delimiter=",")
+
+# f = numpy.fromfile('x.csv',sep=',')
+# print(f)
+
+# import csv
+# with open('x.csv', 'r') as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         print(row)
+
+# f = open('x.csv','r')
+# data = [x.strip() for x in f.readlines()]
+# print(data)
+# f.close()
+
+# x = np.array(data)
+# print(x)
+# x = np.array(list(range(10)))
+# print(x)
+
+x = '0.00012477'
+# print(int(x))
+# print(real(x))
+print(complex(x))
+print(float(x))
--- mapping.py
+++ mapping.py
@@ -3,37 +3,57 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D 
 import numpy as np
 
-Base = -1
-n = 10 #range of x
-density = 10 # intervals
+Base = -2
+n = 15 #range of x
+density = 100 # intervals
+PrintData = 0# True # 1 or 0
 
-plt.rcParams['figure.figsize']=(15,7.5)
-fig = plt.figure(f'Mapping Im & Re : for {Base}^x, x_domain {n}, density {density}')
-ax = Axes3D(fig)
+TITLE = f'Mapping Im & Re : for {Base}^x, x_domain {n}, density {density}'
+
+def genData(Base,n,density):
+  x = np.array(list(map(lambda x: x/density,range(n*density))),dtype=complex) # ,dtype=complex means that the array understands it is a complex number and not Nan
+  y = np.array(list(map(
+    lambda y: (Base**y).real,x     
+      )))
+  z = np.array(list(map(
+    lambda z: (Base**z).imag,x       
+      )))
+  return x,y,z
+
+def store(x,y,z):
+ np.savetxt(f"x_{TITLE}.csv", x, delimiter=",")
+ np.savetxt(f"y_{TITLE}.csv", y, delimiter=",")
+ np.savetxt(f"z_{TITLE}.csv", z, delimiter=",")
+store(x,y,z)
+
+def read():
+  values =[]
+  labels=['x','y','z']
+  for label in labels:
+    f = open(f'{label}_{TITLE}.csv','r')
+    data = np.array([complex(x.strip()).real for x in f.readlines()])
+    f.close()
+    values.append(data)
+  return values # x,y,z
 
 
-x = np.array(list(map(lambda x: x/density,range(n*density))),dtype=complex) # ,dtype=complex means that the array understands it is a complex number and not Nan
-# print(x.shape)
-# print(x)
-y = np.array(list(map(
-  lambda y: (Base**y).real,x     
-    )))
-# z = np.random.normal(mu,1, size=n)
-z = np.array(list(map(
-  lambda z: (Base**z).imag,x       
-    )))
-# print('y',y)
-# [print(Base**x1) for x1 in x]
-print('x',x,'\n\ny',y,'\n\nz',z)
+  
+if PrintData: print('x:',x,'\n\ny:',y,'\n\nz:',z)
+
+def plot(x,y,z):
+  plt.rcParams['figure.figsize']=(15,10)
+  fig = plt.figure(TITLE)
+  ax = Axes3D(fig)
+
+  ax.scatter3D(x,z,y)
+  ax.set_xlabel('x')
+  ax.set_zlabel('y (Re) ')  # better orientation for visualisation
+  ax.set_ylabel('z (Im)') # better orientation for visualisation
+
+  plt.show()
+
+# plot(x,y,z)
 
 
-ax.scatter3D(x,y,z)
-ax.set_xlabel('x')
-ax.set_ylabel('y (Re) ')
-ax.set_zlabel('z (Im)')
 
-plt.show()
-
-# y = [(Base**x1) for x1 in x]
-
-
+x2,y2,z2 = read()
